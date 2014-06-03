60bbda054afa6d2d51383efdfb351cca959a3bdd
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-06-02 17:02:43 -0700

    setup initial environment for hw4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,9 +21,16 @@ class MoviesController < ApplicationController
       @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
     end
     
-    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+    if params[:sort] != session[:sort]
+      session[:sort] = sort
+      flash.keep
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+
+    if params[:ratings] != session[:ratings] and @selected_ratings != {}
       session[:sort] = sort
       session[:ratings] = @selected_ratings
+      flash.keep
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
     @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
